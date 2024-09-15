7cff4682c4230493087e9ce203e478141a43ec21
joerg <joerg@example.com>
2024-09-15 18:36:22 +0200

    corrections

--- src/movements/attractor.rs
+++ src/movements/attractor.rs
@@ -1,15 +1,3 @@
-fish: Unknown command: fzf_key_bindings
-~/.config/fish/functions/fish_user_key_bindings.fish (line 1): 
-fzf_key_bindings
-^~~~~~~~~~~~~~~^
-from sourcing file ~/.config/fish/functions/fish_user_key_bindings.fish
-	called on line 1 of file ~/.local/share/omf/init.fish
-from sourcing file ~/.local/share/omf/init.fish
-	called on line 7 of file ~/.config/fish/conf.d/omf.fish
-from sourcing file ~/.config/fish/conf.d/omf.fish
-	called on line 248 of file /usr/share/fish/config.fish
-from sourcing file /usr/share/fish/config.fish
-	called during startup
 use crate::Vec2;
 use rand::Rng;
 
